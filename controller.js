d759ba5736cf96deb8e8f2bd8a6a2092f9938d8e
M.Sc. Damir Olejar <olejar.damir@gmail.com>
2020-12-24 13:36:48 -0500

    Add files via upload

--- controller.js
+++ controller.js
@@ -7,7 +7,41 @@ module.exports = http.createServer((req, res) => {
     const reqUrl = url.parse(req.url, true);
 
     // GET Endpoint
-    if (reqUrl.pathname == '/sample' && req.method === 'GET') {
+    if (reqUrl.pathname == '/listRecipes' && req.method === 'GET') {
+        console.log('Request Type:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+
+        service.listRecipes(req, res);
+    }
+
+    // POST Endpoint
+   else if (reqUrl.pathname == '/addRecipe' && req.method === 'POST') {
+    console.log('Request Type:' +
+        req.method + ' Endpoint: ' +
+        reqUrl.pathname);
+        service.addRecipe(req, res);
+  }
+
+    // POST Endpoint
+    else if (reqUrl.pathname == '/removeRecipe' && req.method === 'DELETE') {
+        console.log('Request Type:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+        service.removeRecipe(req, res);
+    }
+
+    // POST Endpoint
+    else if (reqUrl.pathname == '/updateRecipe' && req.method === 'POST') {
+        console.log('Request Type:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+        service.updateRecipe(req, res);
+    }
+
+
+    // GET Endpoint
+   else if (reqUrl.pathname == '/sample' && req.method === 'GET') {
         console.log('Request Type:' +
             req.method + ' Endpoint: ' +
             reqUrl.pathname);
--- service.js
+++ service.js
@@ -1,22 +1,60 @@
 const url = require('url');
 
-exports.sampleRequest = function (req, res) {
-    const reqUrl = url.parse(req.url, true);
-    var name = 'World';
-    if (reqUrl.query.name) {
-        name = reqUrl.query.name
+/*
+ Recipe example:
+
+ {
+ "id": 0,
+ "name": "Pizza",
+ "instructions": "Blah, blah, blah",
+ "ingredients": "1kg of something"
+ }
+
+ */
+
+
+/*
+GET: List all recipes
+POST: Add a new recipe
+DELETE: Remove a recipe
+POST: Update a recipe
+POST: Manage individual ingredients
+*/
+
+var cookbook = []; //this is where we store recipes
+var pivot = 0;
+
+function verifyRecipe(postBody){
+    var id = postBody.id;
+    if ( verifyRecipeNoId(postBody) && typeof id !== 'undefined'){
+        return true;
     }
+    return false;
+}
 
-    var response = {
-        "text": "Hello " + name
-    };
+function verifyRecipeNoId(postBody){
+    var name = postBody.name;
+    var instructions = postBody.instructions;
+    var ingredients = postBody.ingredients;
+    if (typeof name !== 'undefined' &&
+        typeof instructions !== 'undefined' &&
+        typeof ingredients !== 'undefined'
+    ){
+        return true;
+    }
+        return false;
+}
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(response));
-};
+//--------------------LIST RECIPES-------------------------------------
+//GET
+exports.listRecipes = function (req, res){
+    const reqUrl = url.parse(req.url, true);
+    OKResponse(res, cleanCookBook());
+}
 
-exports.testRequest = function (req, res) {
+//--------------------ADD RECIPE-------------------------------------
+//POST
+exports.addRecipe = function(req,res){
     body = '';
 
     req.on('data', function (chunk) {
@@ -24,18 +62,122 @@ exports.testRequest = function (req, res) {
     });
 
     req.on('end', function () {
+        postBody = JSON.parse(body);
+        if(!verifyRecipeNoId(postBody)){
+            var response = {
+                "Response": "Incomplete recipe " + postBody.name
+            };
+            badRequest(res,response);
+        }
+        else {
+            postBody.id = pivot;
+            cookbook[pivot] = postBody;
+            pivot = pivot+1;
 
+            var response = {
+                "Response": "Added new recipe at id: "+(pivot-1)
+            };
+            OKResponse(res, response);
+        }
+    });
+}
+
+//--------------------DELETE RECIPE-------------------------------------
+//DELETE
+exports.removeRecipe = function (req,res){
+    body = '';
+
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function () {
+        postBody = JSON.parse(body);
+        if( typeof cookbook[postBody.id] == 'undefined'){
+            var response = {
+                "Response": "Invalid recipe id:" + postBody.id
+            };
+            badRequest(res,response);
+        }
+        else {
+            var id = postBody.id;
+            cookbook[id] = undefined;
+            var response = {
+                "Response": "Removed recipe at id: "+id
+            };
+            OKResponse(res, response);
+        }
+    });
+
+
+}
+
+//--------------------UPDATE RECIPE-------------------------------------
+//POST
+exports.updateRecipe = function (req,res){
+    body = '';
+
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function () {
         postBody = JSON.parse(body);
 
-        var response = {
-            "text": "Post Request Value is  " + postBody.value
-        };
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        if(!verifyRecipe(postBody)){
+            var response = {
+                "Response": "Incomplete recipe " + postBody.name
+            };
+            badRequest(res,response);
+        }
+        else if( typeof cookbook[postBody.id] == 'undefined'){
+            var response = {
+                "Response": "Invalid recipe id:" + postBody.id
+            };
+            badRequest(res,response);
+        }
+        else {
+            var id = postBody.id;
+            cookbook[id] = postBody;
+            var response = {
+                "Response": "Updated recipe at id: "+id
+            };
+            OKResponse(res, response);
+        }
     });
-};
+
+
+}
+
+//--------------------MANAGE INGREDIENTS-------------------------------------
+//POST
+exports.manageIngredients = function (req,ser){}
+
+//----------------------------------------------------------------------------
+
+function cleanCookBook(){
+    var ret = [];
+    var c = 0;
+    for(i=0;i<pivot;i++){
+        if(cookbook[i]!=null){
+            ret[c] = cookbook[i];
+            c++;
+        }
+    }
+    return ret;
+}
+
+function OKResponse(res, response) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+}
+
+function badRequest(res, response) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+}
 
 exports.invalidRequest = function (req, res) {
     res.statusCode = 404;
